56bdf020ea818317accc16ec4366b622699d3dbc
galohot <114443831+galohot@users.noreply.github.com>
2024-08-01 08:51:17 +0700

    reject logic

--- confirmbutton.php
+++ confirmbutton.php
@@ -22,6 +22,14 @@ class PlgSystemConfirmButton extends CMSPlugin
         {
             $this->rescindItem();
         }
+        elseif ($task === 'rejectItem')
+        {
+            $this->rejectItem();
+        }
+        elseif ($task === 'rescindRejectItem')
+        {
+            $this->rescindRejectItem();
+        }
     }
 
     private function confirmItem()
@@ -31,48 +39,28 @@ class PlgSystemConfirmButton extends CMSPlugin
         $email = $input->getString('email');
         $fullName = $input->getString('fullname');
         $table = $input->getString('tbl');
+        $quota = $input->getInt('quota', $this->params->get('quota', 9999)); // Get quota from params
 
         if ($id && $email && $fullName && $table)
         {
             $db = Factory::getDbo();
 
-            // Check if records are over 1000
+            // Check if records are over the quota
             $query = $db->getQuery(true)
                         ->select('COUNT(*)')
                         ->from($db->quoteName($table))
-                        ->where($db->quoteName('status') . ' = ' . $db->quote('approved')); // Modified line
+                        ->where($db->quoteName('status') . ' = ' . $db->quote('approved'));
             $db->setQuery($query);
             $count = $db->loadResult();
 
-            if ($count > 1000) {
+            if ($count > $quota) {
                 // Set status as 'waiting list'
                 $status = 'waiting list';
-                $messageBody = "Dear $fullName,
-
-Thank you for registering for the 2nd Indonesia-Africa Forum (IAF).
-Due to overwhelming interest, we have reached our full capacity, and your
-registration is now on the waiting list.
-To follow up on the spot availability, please reach out to us by email at iaf.reg@kemlu.go.id.
-
-Warm regards,
-The 2nd IAF Registration Committee";
+                $messageTemplate = $this->params->get('waiting_list_message', 'Default waiting list message');
             } else {
                 // Set status as 'approved'
                 $status = 'approved';
-                $messageBody = "Dear $fullName,
-
-Thank you for registering for the 2nd Indonesia-Africa Forum (IAF). Your participation is confirmed for the event in Bali scheduled from September 1-3, 2024.
-
-Please visit our website at iaf.kemlu.go.id for the latest updates on the program, concept note, administrative details, and more. Please be advised that some programs require special access to attend.
-
-Should you require assistance with visa procedures, don't hesitate to get in touch with the Indonesian Embassy in your country.
-
-For any further questions, feel free to email us at iaf.reg@kemlu.go.id.
-
-We are looking forward to welcoming you to Bali.
-
-Warm regards,
-The 2nd IAF Registration Committee";
+                $messageTemplate = $this->params->get('approved_message', 'Default approved message');
             }
 
             $query = $db->getQuery(true)
@@ -81,6 +69,13 @@ The 2nd IAF Registration Committee";
                         ->where($db->quoteName('id') . ' = ' . $db->quote($id));
             $db->setQuery($query);
             $db->execute();
+
+            // Replace placeholders with actual values
+            $messageBody = str_replace(
+                ['{fullname}'],
+                [$fullName],
+                $messageTemplate
+            );
 
             // Send confirmation email
             $mailer = Factory::getMailer();
@@ -93,7 +88,7 @@ The 2nd IAF Registration Committee";
 
             $mailer->setSender($sender);
             $mailer->addRecipient($email);
-            $mailer->setSubject('Confirmation');
+            $mailer->setSubject($this->params->get('confirmation_subject', 'The Second IAF Registration'));
             $mailer->setBody($messageBody);
 
             if ($mailer->Send() !== true)
@@ -142,12 +137,14 @@ The 2nd IAF Registration Committee";
                 $email = $result->email;
                 $fullName = $result->fullname;
 
-                $messageBody = "Dear $fullName,
+                $messageTemplate = $this->params->get('rescinded_message', 'Default rescinded message');
 
-We regret to inform you that your status for the 2nd Indonesia-Africa Forum (IAF) has been rescinded. Please contact us at iaf.reg@kemlu.go.id if you have any questions or need further assistance.
-
-Warm regards,
-The 2nd IAF Registration Committee";
+                // Replace placeholders with actual values
+                $messageBody = str_replace(
+                    ['{fullname}'],
+                    [$fullName],
+                    $messageTemplate
+                );
 
                 $mailer = Factory::getMailer();
                 $config = Factory::getConfig();
@@ -159,7 +156,7 @@ The 2nd IAF Registration Committee";
 
                 $mailer->setSender($sender);
                 $mailer->addRecipient($email);
-                $mailer->setSubject('Status Rescinded');
+                $mailer->setSubject($this->params->get('rescinded_subject', 'The Second IAF Registration'));
                 $mailer->setBody($messageBody);
 
                 if ($mailer->Send() !== true)
@@ -175,6 +172,122 @@ The 2nd IAF Registration Committee";
         else
         {
             Factory::getApplication()->enqueueMessage('Missing ID or table name', 'error');
+        }
+
+        $app = Factory::getApplication();
+        $app->redirect(JRoute::_(Factory::getURI()->toString(), false));
+    }
+
+    private function rejectItem()
+    {
+        $input = Factory::getApplication()->input;
+        $id = $input->getInt('id');
+        $email = $input->getString('email');
+        $fullName = $input->getString('fullname');
+        $table = $input->getString('tbl');
+
+        if ($id && $email && $fullName && $table)
+        {
+            $db = Factory::getDbo();
+            $query = $db->getQuery(true)
+                        ->update($db->quoteName($table))
+                        ->set($db->quoteName('status') . ' = ' . $db->quote('rejected'))
+                        ->where($db->quoteName('id') . ' = ' . $db->quote($id));
+            $db->setQuery($query);
+            $db->execute();
+
+            // Replace placeholders with actual values
+            $messageTemplate = $this->params->get('rejected_message', 'Default rejected message');
+            $messageBody = str_replace(
+                ['{fullname}'],
+                [$fullName],
+                $messageTemplate
+            );
+
+            // Send rejection email
+            $mailer = Factory::getMailer();
+            $config = Factory::getConfig();
+
+            $sender = array(
+                $config->get('mailfrom'),
+                $config->get('fromname')
+            );
+
+            $mailer->setSender($sender);
+            $mailer->addRecipient($email);
+            $mailer->setSubject($this->params->get('rejection_subject', 'The Second IAF Registration'));
+            $mailer->setBody($messageBody);
+
+            if ($mailer->Send() !== true)
+            {
+                Factory::getApplication()->enqueueMessage('Error sending email', 'error');
+            }
+            else
+            {
+                Factory::getApplication()->enqueueMessage('Status updated and email sent', 'message');
+            }
+        }
+        else
+        {
+            Factory::getApplication()->enqueueMessage('Missing ID, email, fullname, or table name', 'error');
+        }
+
+        $app = Factory::getApplication();
+        $app->redirect(JRoute::_(Factory::getURI()->toString(), false));
+    }
+
+    private function rescindRejectItem()
+    {
+        $input = Factory::getApplication()->input;
+        $id = $input->getInt('id');
+        $email = $input->getString('email');
+        $fullName = $input->getString('fullname');
+        $table = $input->getString('tbl');
+
+        if ($id && $email && $fullName && $table)
+        {
+            $db = Factory::getDbo();
+            $query = $db->getQuery(true)
+                        ->update($db->quoteName($table))
+                        ->set($db->quoteName('status') . ' = ' . $db->quote('pending'))
+                        ->where($db->quoteName('id') . ' = ' . $db->quote($id));
+            $db->setQuery($query);
+            $db->execute();
+
+            // Replace placeholders with actual values
+            $messageTemplate = $this->params->get('rescind_reject_message', 'Default rescind reject message');
+            $messageBody = str_replace(
+                ['{fullname}'],
+                [$fullName],
+                $messageTemplate
+            );
+
+            // Send rescind reject email
+            $mailer = Factory::getMailer();
+            $config = Factory::getConfig();
+
+            $sender = array(
+                $config->get('mailfrom'),
+                $config->get('fromname')
+            );
+
+            $mailer->setSender($sender);
+            $mailer->addRecipient($email);
+            $mailer->setSubject($this->params->get('rescind_reject_subject', 'The Second IAF Registration'));
+            $mailer->setBody($messageBody);
+
+            if ($mailer->Send() !== true)
+            {
+                Factory::getApplication()->enqueueMessage('Error sending email', 'error');
+            }
+            else
+            {
+                Factory::getApplication()->enqueueMessage('Status updated and email sent', 'message');
+            }
+        }
+        else
+        {
+            Factory::getApplication()->enqueueMessage('Missing ID, email, fullname, or table name', 'error');
         }
 
         $app = Factory::getApplication();
